c689c1438fb9027882ea6376a66396f34ae7f19c
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2026-02-27 17:55:37 +0200

    Add AppUser and ShortenedUrl entities

---
+++ UrlShortener.DAL/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UrlShortener.DAL.Entities;
+
+public class AppUser : IdentityUser
+{
+    public DateTime CreatedAt { get; set; }
+
+    public ICollection<ShortenedUrl> ShortenedUrls { get; set; } = new List<ShortenedUrl>();
+}
