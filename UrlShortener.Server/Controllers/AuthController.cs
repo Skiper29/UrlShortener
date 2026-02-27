d5947d65599eff40876cb667c6ca9aad019221a5
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2026-02-28 01:24:19 +0200

    Add AuthController with login/register endpoints

---
+++ UrlShortener.Server/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShortener.BLL.DTOs.Auth;
+using UrlShortener.BLL.Features.Auth.Commands.Login;
+using UrlShortener.BLL.Features.Auth.Commands.Register;
+using UrlShortener.Server.Controllers.Common;
+
+namespace UrlShortener.Server.Controllers;
+
+
+public class AuthController : BaseApiController
+{
+    [HttpPost("login")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AuthResponse))]
+    public async Task<ActionResult<AuthResponse>> Login([FromBody] LoginRequest request)
+    {
+        return HandleResult(await Mediator.Send(new LoginCommand(request)));
+    }
+
+    [HttpPost("register")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AuthResponse))]
+    public async Task<ActionResult<AuthResponse>> Register([FromBody] RegisterRequest request)
+    {
+        return HandleResult(await Mediator.Send(new RegisterCommand(request)));
+    }
+}
--- UrlShortener.Server/Controllers/Common/BaseApiController.cs
+++ UrlShortener.Server/Controllers/Common/BaseApiController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc.Infrastructure;
 namespace UrlShortener.Server.Controllers.Common;
 
 [ApiController]
+[Route("api/[controller]")]
 public class BaseApiController : ControllerBase
 {
     private IMediator? _mediator;
