979ae50eae4af2488175d86ef9bea1fab6902e22
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2026-02-27 18:04:08 +0200

    Add AppDbContext with Identity and ShortenedUrl support

---
+++ UrlShortener.DAL/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.DAL.Entities;
+
+namespace UrlShortener.DAL.Data;
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<ShortenedUrl> ShortenedUrls { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
