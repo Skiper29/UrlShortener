a20b1580665a840d3bfcd1ee87bcc5f4ee4df29a
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2026-02-27 19:41:52 +0200

    Add RepositoryWrapper and UrlRepository implementations

---
+++ UrlShortener.DAL/Repositories/Realizations/Base/RepositoryWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System.Transactions;
+using UrlShortener.DAL.Data;
+using UrlShortener.DAL.Repositories.Interfaces;
+using UrlShortener.DAL.Repositories.Interfaces.Base;
+
+namespace UrlShortener.DAL.Repositories.Realizations.Base;
+
+public class RepositoryWrapper : IRepositoryWrapper
+{
+    private readonly AppDbContext _dbContext;
+    private IUrlRepository? _urlRepository;
+
+    public RepositoryWrapper(AppDbContext context)
+    {
+        _dbContext = context;
+    }
+
+    public IUrlRepository UrlRepository => _urlRepository ??= new UrlRepository(_dbContext);
+
+    public int SaveChanges()
+    {
+        return _dbContext.SaveChanges();
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _dbContext.SaveChangesAsync();
+    }
+
+    public TransactionScope BeginTransaction()
+    {
+        return new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+    }
+}
---
+++ UrlShortener.DAL/Repositories/Realizations/UrlRepository.cs
@@ -0,0 +1,13 @@
+﻿using UrlShortener.DAL.Data;
+using UrlShortener.DAL.Entities;
+using UrlShortener.DAL.Repositories.Interfaces;
+using UrlShortener.DAL.Repositories.Realizations.Base;
+
+namespace UrlShortener.DAL.Repositories.Realizations;
+
+public class UrlRepository : RepositoryBase<ShortenedUrl>, IUrlRepository
+{
+    public UrlRepository(AppDbContext context) : base(context)
+    {
+    }
+}
