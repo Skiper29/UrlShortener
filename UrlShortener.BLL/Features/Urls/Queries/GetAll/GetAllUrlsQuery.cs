9d43c29d612e20bfae7e8b6e07b972c8c8da340e
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2026-02-27 23:44:01 +0200

    Add GetAllUrls query and handler for URL retrieval

---
+++ UrlShortener.BLL/Features/Urls/Queries/GetAll/GetAllUrlsQuery.cs
@@ -0,0 +1,7 @@
+﻿using FluentResults;
+using MediatR;
+using UrlShortener.BLL.DTOs.Urls;
+
+namespace UrlShortener.BLL.Features.Urls.Queries.GetAll;
+
+public record GetAllUrlsQuery() : IRequest<Result<IEnumerable<UrlResponse>>>;
---
+++ UrlShortener.BLL/Features/Urls/Queries/GetAll/GetAllUrlsQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using UrlShortener.BLL.DTOs.Urls;
+using UrlShortener.DAL.Repositories.Interfaces.Base;
+
+namespace UrlShortener.BLL.Features.Urls.Queries.GetAll;
+
+public class GetAllUrlsQueryHandler : IRequestHandler<GetAllUrlsQuery, Result<IEnumerable<UrlResponse>>>
+{
+    private readonly IRepositoryWrapper _repository;
+    private readonly IMapper _mapper;
+
+    public GetAllUrlsQueryHandler(IRepositoryWrapper repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<IEnumerable<UrlResponse>>> Handle(GetAllUrlsQuery request, CancellationToken cancellationToken)
+    {
+        var urls = await _repository.UrlRepository.GetAllAsync();
+
+        return Result.Ok(urls.Select(url =>
+        {
+            var dto = _mapper.Map<UrlResponse>(url);
+            return dto with { ShortUrl = $"/r/{url.ShortCode}" };
+        }));
+    }
+}
