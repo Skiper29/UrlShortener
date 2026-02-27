d8424029d975d2700d646d9e3174ed21450ecba8
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2026-02-27 23:37:02 +0200

    Add command/handler for deleting shortened URLs

---
+++ UrlShortener.BLL/Features/Urls/Commands/Delete/DeleteShortUrlCommand.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace UrlShortener.BLL.Features.Urls.Commands.Delete;
+
+public record DeleteShortUrlCommand(int Id, string UserId) : IRequest<Result<Unit>>;
---
+++ UrlShortener.BLL/Features/Urls/Commands/Delete/DeleteShortUrlCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using FluentResults;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.DAL.Entities;
+using UrlShortener.DAL.Enums;
+using UrlShortener.DAL.Repositories.Interfaces.Base;
+using UrlShortener.DAL.Repositories.Options;
+
+namespace UrlShortener.BLL.Features.Urls.Commands.Delete;
+
+public class DeleteShortUrlCommandHandler : IRequestHandler<DeleteShortUrlCommand, Result<Unit>>
+{
+    private readonly IRepositoryWrapper _repository;
+    private readonly UserManager<AppUser> _userManager;
+
+    public DeleteShortUrlCommandHandler(IRepositoryWrapper repository, UserManager<AppUser> userManager)
+    {
+        _repository = repository;
+        _userManager = userManager;
+    }
+
+    public async Task<Result<Unit>> Handle(DeleteShortUrlCommand request, CancellationToken cancellationToken)
+    {
+        var url = await _repository.UrlRepository.GetFirstOrDefaultAsync(new QueryOptions<ShortenedUrl>
+        {
+            Filter = u => u.Id == request.Id,
+            Include = u => u.Include(u => u.CreatedBy)
+        });
+
+        if (url is null)
+        {
+            return Result.Fail("URL not found.");
+        }
+
+        var user = await _userManager.FindByIdAsync(request.UserId);
+
+        if (user is null)
+        {
+            return Result.Fail("User not found.");
+        }
+
+        var isAdmin = await _userManager.IsInRoleAsync(user, AppRole.Admin.ToString());
+
+        if (!isAdmin && url.CreatedById != request.UserId)
+        {
+            return Result.Fail("You do not have permission to delete this URL.");
+        }
+
+        _repository.UrlRepository.Delete(url);
+
+        var result = await _repository.SaveChangesAsync();
+
+        if (result > 0)
+        {
+            return Result.Ok();
+        }
+
+        return Result.Fail("Failed to delete the URL.");
+    }
+}
