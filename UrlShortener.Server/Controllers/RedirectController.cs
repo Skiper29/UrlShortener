3eda7019b0810492a8876de1ba5dfb0e88bc1186
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2026-02-28 12:59:12 +0200

    Add RedirectController for short URL redirection

---
+++ UrlShortener.Server/Controllers/RedirectController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UrlShortener.BLL.Features.Urls.Queries.GetByShortCode;
+using UrlShortener.Server.Controllers.Common;
+
+namespace UrlShortener.Server.Controllers;
+
+public class RedirectController : BaseApiController
+{
+    [HttpGet("{shortCode}")]
+    [AllowAnonymous]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status302Found)]
+    public async Task<IActionResult> RedirectToOriginalUrl(string shortCode)
+    {
+        var result = await Mediator.Send(new GetUrlByShortCodeQuery(shortCode));
+        if (result.IsSuccess)
+        {
+            var originalUrl = result.Value.OriginalUrl;
+            return Redirect(originalUrl);
+        }
+        return NotFound();
+    }
+}
