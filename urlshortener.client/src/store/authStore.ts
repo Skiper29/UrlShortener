d5c2e15eeb28011b369c98ffaf51af8e8e997cf6
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2026-02-28 14:54:45 +0200

    Add authStore for managing authentication state with Zustand

---
+++ urlshortener.client/src/store/authStore.ts
@@ -0,0 +1,36 @@
+import {create} from "zustand";
+import type {AuthUser} from "../types/auth.types.ts";
+import {persist} from "zustand/middleware/persist";
+
+
+interface AuthState {
+    user: AuthUser | null;
+    token: string | null;
+    isAuthenticated: boolean;
+    setAuth: (user: AuthUser, token: string) => void;
+    logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>()(
+    persist(
+        (set) => ({
+            user: null,
+            token: null,
+            isAuthenticated: false,
+
+            setAuth: (user, token) => {
+                localStorage.setItem('token', token);
+                set({ user, token, isAuthenticated: true });
+            },
+            logout: () => {
+                localStorage.removeItem('token');
+                set({ user: null, token: null, isAuthenticated: false });
+            },
+        }),
+        {
+            name: 'auth-storage',
+            partialize: (state) => ({ user: state.user, token: state.token }),
+
+        }
+    )
+);
