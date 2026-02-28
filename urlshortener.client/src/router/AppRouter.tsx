8a0bc9fe74068731f7ce5ac3fb256b09a49af2df
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2026-02-28 17:39:31 +0200

    Refactor App component to use AppRouter and remove weather data fetching logic

--- urlshortener.client/src/router/AppRouter.tsx
+++ urlshortener.client/src/router/AppRouter.tsx
@@ -1,8 +1,10 @@
-import { BrowserRouter, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AuthPage} from "../features/auth/pages/AuthPage.tsx";
 
 export const AppRouter = () => (
     <BrowserRouter>
         <Routes>
+            <Route path="/auth" element={<AuthPage />} />
         </Routes>
     </BrowserRouter>
 );
