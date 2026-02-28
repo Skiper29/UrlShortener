b24ca406d8e7bb7c9edf750bcf59f662bff0bf32
Vitalii Demkiv <61919416+Skiper29@users.noreply.github.com>
2026-02-28 12:52:16 +0200

    Add query & handler to get URL details by short code

---
+++ UrlShortener.BLL/Features/Urls/Queries/GetByShortCode/GetUrlByShortCodeQuery.cs
@@ -0,0 +1,7 @@
+﻿using FluentResults;
+using MediatR;
+using UrlShortener.BLL.DTOs.Urls;
+
+namespace UrlShortener.BLL.Features.Urls.Queries.GetByShortCode;
+
+public record GetUrlByShortCodeQuery(string ShortCode) : IRequest<Result<UrlDetailResponse>>;
---
+++ UrlShortener.BLL/Features/Urls/Queries/GetByShortCode/GetUrlByShortCodeQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.BLL.DTOs.Urls;
+using UrlShortener.DAL.Entities;
+using UrlShortener.DAL.Repositories.Interfaces.Base;
+using UrlShortener.DAL.Repositories.Options;
+
+namespace UrlShortener.BLL.Features.Urls.Queries.GetByShortCode;
+
+public class GetUrlByShortCodeQueryHandler : IRequestHandler<GetUrlByShortCodeQuery, Result<UrlDetailResponse>>
+{
+    private readonly IRepositoryWrapper _repository;
+    private readonly IMapper _mapper;
+
+    public GetUrlByShortCodeQueryHandler(IRepositoryWrapper repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<UrlDetailResponse>> Handle(GetUrlByShortCodeQuery request, CancellationToken cancellationToken)
+    {
+        var url = await _repository.UrlRepository.GetFirstOrDefaultAsync(new QueryOptions<ShortenedUrl>
+        {
+            Filter = u => u.ShortCode == request.ShortCode,
+            Include = q => q.Include(u => u.CreatedBy)
+        });
+
+        if (url is null)
+        {
+            return Result.Fail<UrlDetailResponse>("URL not found.");
+        }
+
+        var dto = _mapper.Map<UrlDetailResponse>(url);
+        return Result.Ok(dto with { ShortUrl = $"/r/{url.ShortCode}" });
+    }
+}
